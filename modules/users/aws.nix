2ed60658d732538b5bbb9125aa87bd3d44c8f10a
Thomas Christensen <thomas@chrstnsn.dk>
2021-10-13 18:08:41 +0200

    add aws

---
+++ modules/users/aws.nix
@@ -0,0 +1,27 @@
+{ pkgs, config, lib, ... }:
+with lib;
+
+let 
+  cfg = config.tc.aws;
+in {
+  options.tc.aws = {
+    enable = mkOption {
+      description = "Enable aws + terraform";
+      type = types.bool;
+      default = true;
+    };
+  };
+
+  config = mkIf (cfg.enable) {
+    home.packages = with pkgs; [
+      awscli2
+      terraform
+    ];
+
+    programs.zsh.sessionVariables = {
+      AWS_PROFILE = "playground";
+    };
+
+    programs.zsh.oh-my-zsh.plugins = [ "terraform" ];
+  };
+}
