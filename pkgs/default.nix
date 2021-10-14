8b504cea8196757628ccfee1bb0dcbb7529da576
Thomas Christensen <thomas@chrstnsn.dk>
2021-10-14 19:00:08 +0000

    sha512 for dotnet-sdk_2.2

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -12,8 +12,8 @@ in
      dotnet.sdk_2_2 = buildNetCoreSdk {
         version = "2.2.207";
         sha512 = {
-          x86_64-linux = "000";
-          aarch64-linux = "000";
+          x86_64-linux  = "sha512-nXC0qKY7ZtqQVECHGZoPaB0TW/kNQ8pTsS6pfMYAp2iwo9L4JM/ie9MijgWLBgxjMZzYYDO+i40nklKD+Z3pWA==;";
+          aarch64-linux = lib.fakeSha512;
           x86_64-darwin = "2pafmx09mjri25v4xidnhhnbygy1r9bw8sj0z5mp2blw2nc5q9ddqzp8k51sjscwv3mxzhy3vqsc8wj44swdj6a1brsh25sa4w6h3fn";
         };
     };
