1c60bc3a293bab5f1c5c8210cadf8d93a1441ffd
Thomas Christensen <thomas@chrstnsn.dk>
2021-10-26 20:24:44 +0200

    moar osx tweaks

--- modules/darwin/osx.nix
+++ modules/darwin/osx.nix
@@ -1,10 +1,20 @@
 { pkgs, lib, ... }:
 
 {
+  system.defaults.finder = {
+    AppleShowAllExtensions = true;
+    _FXShowPosixPathInTitle = true;
+  };
+
   system.defaults.dock = {
     orientation = "right";
     autohide = true;
+    mru-spaces = false;
   };
+
+  system.defaults.spaces.spans-displays = false;
+
+  system.defaults.screencapture.location = "~/Desktop/screenshots";
 
   system.defaults.NSGlobalDomain = {
     InitialKeyRepeat = 15;
