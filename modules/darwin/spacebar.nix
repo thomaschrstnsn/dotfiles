9b3183e4b6f486aff144d0d0cb27e8d1fc4741d2
Thomas Christensen <thomas.christensen@schibsted.com>
2021-10-28 08:40:12 +0200

    tweak accent

--- modules/darwin/spacebar.nix
+++ modules/darwin/spacebar.nix
@@ -28,7 +28,8 @@
     spaces_for_all_displays    = "on";
     display_separator          = "on";
     display_separator_icon     = "";
-    space_icon_color           = "0xff458588";
+    #space_icon_color           = "0xff458588";
+    space_icon_color           = "0xfff55f42"; # keychron k2v2 accent
     space_icon_color_secondary = "0xff78c4d4";
     space_icon_color_tertiary  = "0xfffff9b0";
     clock_icon                 = "";
