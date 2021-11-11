4cd80aa3cf17ba887d365f96876a6af10c528ad8
Thomas Christensen <thomas@chrstnsn.dk>
2021-11-11 22:07:36 +0100

    'final' breaking refactor in this round 🤞

--- flake.nix
+++ flake.nix
@@ -28,13 +28,6 @@
     let
       inherit (nixpkgs) lib;
 
-      systems = {
-        arm-linux = "aarch64-linux";
-        x64-linux = "x86_64-linux";
-        m1_darwin = "aarch64-darwin";
-        x64-darwin = "x86_64-darwin";
-      };
-
       mkDarwinSystem =
         { extraModules ? [ ]
         , system
@@ -126,119 +119,7 @@
           }
         );
 
-      # MACHINE CONFIGURATIONS
-
-      machines = {
-        aeris = {
-          home = {
-            user = rec {
-              username = "thomas";
-              homedir = "/Users/${username}";
-            };
-            dotnet.enable = true;
-            git.enable = true;
-            haskell.stack.enable = true;
-            haskell.ihp.enable = true;
-            zsh = {
-              enable = true;
-            };
-          };
-
-          darwin = {
-            skhd.enable = true;
-            spacebar.enable = true;
-            yabai.enable = true;
-          };
-
-          extraPackages = pkgs: with pkgs; [
-            shellcheck
-            rnix-lsp
-            nixpkgs-fmt
-          ];
-
-          system = systems.x64-darwin; # actually m1
-        };
-
-        A125228-DK = {
-          home = rec {
-            user = rec {
-              username = "thomas.christensen@schibsted.com";
-              homedir = "/Users/${username}";
-            };
-            aws.enable = true;
-            dotnet.enable = true;
-            git = {
-              enable = true;
-              userEmail = user.username;
-              githubs = [ "github.com" "github.schibsted.io" ];
-            };
-            zsh = {
-              enable = true;
-              skhd = true;
-            };
-            nodejs = {
-              enable = true;
-              pkg = pkgs: pkgs.nodejs-14_x;
-            };
-          };
-
-          darwin = {
-            skhd = {
-              enable = true;
-              browser = "Microsoft Edge";
-              extraAppShortcuts = {
-                "hyper - z" = "zoom.us";
-                "hyper - c" = "Slack";
-                "hyper - r" = "Rider";
-              };
-            };
-            spacebar.enable = true;
-            yabai.enable = true;
-          };
-
-          system = systems.x64-darwin;
-        };
-
-        DESKTOP-IP1G00V = {
-          home = {
-            user = {
-              username = "nixos";
-              homedir = "/home/nixos";
-            };
-            aws.enable = false;
-            dotnet.enable = false;
-            git.enable = true;
-            haskell.stack.enable = false;
-            haskell.ihp.enable = false;
-            zsh = {
-              enable = true;
-              editor = "vim";
-            };
-          };
-          system = systems.x64-linux;
-        };
-
-        nixos-raspi-4 = {
-          home = {
-            user = {
-              username = "pi";
-              homedir = "/home/pi";
-            };
-            git.enable = true;
-            zsh = {
-              enable = true;
-              editor = "vim";
-            };
-            tmux.enable = true;
-          };
-          system = systems.arm-linux;
-        };
-
-        # Minimal configuration to bootstrap darwin systems
-        bootstrap = {
-          system = systems.x64-darwin;
-        };
-      };
+      inherit (import ./machines.nix) machines;
     in
     rec {
       homeManagerConfigurations =
