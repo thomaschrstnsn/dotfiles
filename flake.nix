12755dc258f7af7065473cb83cea11f7260ca0cc
Thomas Christensen <thomas@chrstnsn.dk>
2021-11-11 21:11:31 +0100

    refactor even more

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
       mkDarwinSystem =
         { extraModules ? [ ]
         , system
-        , config
+        , config ? { }
         }:
         darwin.lib.darwinSystem {
           inherit system;
@@ -99,141 +99,144 @@
         in
         { inherit pkgs overlays; };
 
+      machineToHome =
+        (machine:
+          { home ? null
+          , extraPackages ? _: [ ]
+          , system
+          , ...
+          }:
+          {
+            "${builtins.replaceStrings ["."] ["_"] home.user.username}" = mkHMUser {
+              homeConfig = home;
+              extraPackages = extraPackages;
+              system = system;
+            };
+          }
+        );
+
+      machineToDarwin =
+        (machine:
+          { system
+          , darwin ? { }
+          , ...
+          }: mkDarwinSystem {
+            system = system;
+            config = darwin;
+          }
+        );
+
       # MACHINE CONFIGURATIONS
 
-      aeris = {
-        home = {
-          user = {
-            username = "thomas";
-            homedir = "/Users/thomas";
+      machines = {
+        aeris = {
+          home = {
+            user = {
+              username = "thomas";
+              homedir = "/Users/thomas";
+            };
+            dotnet.enable = true;
+            git.enable = true;
+            haskell.stack.enable = true;
+            haskell.ihp.enable = true;
+            zsh = {
+              enable = true;
+            };
           };
-          dotnet.enable = true;
-          git.enable = true;
-          haskell.stack.enable = true;
-          haskell.ihp.enable = true;
-          zsh = {
-            enable = true;
+
+          darwin = {
+            skhd.enable = true;
+            spacebar.enable = true;
+            yabai.enable = true;
           };
+
+          extraPackages = pkgs: with pkgs; [
+            shellcheck
+            rnix-lsp
+            nixpkgs-fmt
+          ];
+
+          system = systems.x64-darwin; # actually m1
         };
 
-        darwin = {
-          skhd.enable = true;
-          spacebar.enable = true;
-          yabai.enable = true;
-        };
-
-        extraPackages = pkgs: with pkgs; [
-          shellcheck
-          rnix-lsp
-          nixpkgs-fmt
-        ];
-
-        system = systems.x64-darwin; # actually m1
-      };
-
-      A125228-DK = {
-        home = {
-          user = {
-            username = "thomas.christensen@schibsted.com";
-            homedir = "/Users/thomas.christensen@schibsted.com";
+        A125228-DK = {
+          home = {
+            user = {
+              username = "thomas.christensen@schibsted.com";
+              homedir = "/Users/thomas.christensen@schibsted.com";
+            };
+            aws.enable = true;
+            dotnet.enable = true;
+            git = {
+              enable = true;
+              userEmail = "thomas.christensen@schibsted.com";
+              githubs = [ "github.com" "github.schibsted.io" ];
+            };
+            zsh = {
+              enable = true;
+              skhd = true;
+            };
+            nodejs = {
+              enable = true;
+              pkg = pkgs: pkgs.nodejs-14_x;
+            };
           };
-          aws.enable = true;
-          dotnet.enable = true;
-          git = {
-            enable = true;
-            userEmail = "thomas.christensen@schibsted.com";
-            githubs = [ "github.com" "github.schibsted.io" ];
-          };
-          zsh = {
-            enable = true;
-            skhd = true;
-          };
-          nodejs = {
-            enable = true;
-            pkg = pkgs: pkgs.nodejs-14_x;
-          };
-        };
 
-        darwin = {
-          skhd.enable = true;
-          spacebar.enable = true;
-          yabai.enable = true;
-        };
-
-        system = systems.x64-darwin;
-      };
-
-      DESKTOP-IP1G00V = {
-        home = {
-          user = {
-            username = "nixos";
-            homedir = "/home/nixos";
+          darwin = {
+            skhd.enable = true;
+            spacebar.enable = true;
+            yabai.enable = true;
           };
-          aws.enable = false;
-          dotnet.enable = false;
-          git.enable = true;
-          haskell.stack.enable = false;
-          haskell.ihp.enable = false;
-          zsh = {
-            enable = true;
-            editor = "vim";
-          };
-        };
-        system = systems.x64-linux;
-      };
 
-      nixos-raspi-4 = {
-        home = {
-          user = {
-            username = "pi";
-            homedir = "/home/pi";
-          };
-          git.enable = true;
-          zsh = {
-            enable = true;
-            editor = "vim";
-          };
-          tmux.enable = true;
-        };
-        system = systems.arm-linux;
-      };
-    in
-    rec {
-      homeManagerConfigurations = {
-        aeris.thomas = mkHMUser {
-          homeConfig = aeris.home;
-          extraPackages = aeris.extraPackages;
-          system = aeris.system;
-        };
-        A125228-DK."thomas_christensen@schibsted_com" = mkHMUser {
-          homeConfig = A125228-DK.home;
-          system = A125228-DK.system;
-        };
-        DESKTOP-IP1G00V.nixos = mkHMUser {
-          homeConfig = DESKTOP-IP1G00V.home;
-          system = DESKTOP-IP1G00V.system;
-        };
-        nixos-raspi-4.pi = mkHMUser {
-          homeConfig = nixos-raspi-4.home;
-          system = nixos-raspi-4.system;
-        };
-      };
-
-      darwinConfigurations = {
-        # Minimal configuration to bootstrap systems
-        bootstrap = mkDarwinSystem {
           system = systems.x64-darwin;
         };
 
-        aeris = mkDarwinSystem {
-          system = aeris.system;
-          config = aeris.darwin;
+        DESKTOP-IP1G00V = {
+          home = {
+            user = {
+              username = "nixos";
+              homedir = "/home/nixos";
+            };
+            aws.enable = false;
+            dotnet.enable = false;
+            git.enable = true;
+            haskell.stack.enable = false;
+            haskell.ihp.enable = false;
+            zsh = {
+              enable = true;
+              editor = "vim";
+            };
+          };
+          system = systems.x64-linux;
         };
 
-        A125228-DK = mkDarwinSystem {
-          system = A125228-DK.system;
-          config = A125228-DK.darwin;
+        nixos-raspi-4 = {
+          home = {
+            user = {
+              username = "pi";
+              homedir = "/home/pi";
+            };
+            git.enable = true;
+            zsh = {
+              enable = true;
+              editor = "vim";
+            };
+            tmux.enable = true;
+          };
+          system = systems.arm-linux;
+        };
+
+        # Minimal configuration to bootstrap darwin systems
+        bootstrap = {
+          system = systems.x64-darwin;
         };
       };
+    in
+    rec {
+      homeManagerConfigurations =
+        builtins.mapAttrs machineToHome machines;
+
+      darwinConfigurations =
+        builtins.mapAttrs machineToDarwin machines;
     };
 }
