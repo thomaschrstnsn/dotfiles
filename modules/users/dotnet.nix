8f4aded60cbd92b79ba2fed6d796024c273a46e0
Thomas Christensen <thomas.christensen@schibsted.com>
2021-11-18 15:11:24 +0100

    dotnet sdk_6

--- modules/users/dotnet.nix
+++ modules/users/dotnet.nix
@@ -18,6 +18,7 @@ in
     home.packages = with pkgs; [
       (with dotnetCorePackages; combinePackages [
         sdk_3_1
+        myPkgs.dotnet.sdk_6_0
         myPkgs.dotnet.sdk_2_2
       ])
     ];
