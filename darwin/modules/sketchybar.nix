94296ec83cb38a0760af8d7d77bade70e9227935
Thomas Christensen <thomas@chrstnsn.dk>
2022-01-03 07:52:36 +0100

    alternative window-title

--- darwin/modules/sketchybar.nix
+++ darwin/modules/sketchybar.nix
@@ -26,82 +26,91 @@ in
     services.sketchybar.enable = true;
     services.sketchybar.package = pkgs.sketchybar;
     services.sketchybar.extraConfig = ''
-      #!/bin/bash
+            #!/bin/bash
 
-      bar_color=0xff2e3440
-      icon_font="MesloLGS NF:Regular:13.0"
-      icon_color=0xbbd8dee9
-      icon_highlight_color=0xffebcb8b
-      label_font="$icon_font"
-      label_color="$icon_color"
-      label_highlight_color="$icon_highlight_color"
+            bar_color=0xff2e3440
+            icon_font="MesloLGS NF:Regular:13.0"
+            icon_color=0xbbd8dee9
+            icon_highlight_color=0xffebcb8b
+            label_font="$icon_font"
+            heavy_font="MesloLGS NF:Bold:13.0"
+            label_color="$icon_color"
+            label_highlight_color="$icon_highlight_color"
 
-      spaces=()
-      for i in {1..8}
-      do
-          spaces+=(--add space space$i left \
-            --set space$i \
-              associated_display=1 \
-              associated_space=$i \
-              icon=$i \
-              click_script="yabai -m space --focus $i" \
-              script=${scripts}/space.sh)
-      done
+            spaces=()
+            for i in {1..8}
+            do
+                spaces+=(--add space space$i left \
+                  --set space$i \
+                    associated_display=1 \
+                    associated_space=$i \
+                    icon=$i \
+                    click_script="yabai -m space --focus $i" \
+                    script=${scripts}/space.sh)
+            done
 
+            sketchybar -m \
+              --bar \
+                height=24 \
+                position=bottom \
+                padding_left=10 \
+                padding_right=10 \
+                color="$bar_color" \
+              --default \
+                cache_scripts=on \
+                icon.font="$icon_font" \
+                icon.color="$icon_color" \
+                icon.highlight_color="$icon_highlight_color" \
+                label.font="$label_font" \
+                label.color="$label_color" \
+                label.highlight_color="$label_highlight_color" \
+                icon.padding_left=10 \
+                icon.padding_right=6 \
+              --add item clock right \
+              --set clock update_freq=10 script="${scripts}/status.sh" icon.padding_left=2 \
+              --add item battery right \
+              --set battery update_freq=60 script="${scripts}/battery.sh" \
+              --add item wifi right \
+              \ #--set wifi click_script="${scripts}/click-wifi.sh" \
+              --add item load right \
+              --set load icon="􀍽" script="${scripts}/window-indicator.sh" \
+              --subscribe load space_change \
+              --add item network right \
+              --default \
+                icon.padding_left=0 \
+                icon.padding_right=2 \
+                label.padding_right=16 \
+              "''${spaces[@]}"
+
+      # from https://github.com/FelixKratz/SketchyBar/discussions/12#discussioncomment-1633997
       sketchybar -m \
-        --bar \
-          height=24 \
-          position=bottom \
-          padding_left=10 \
-          padding_right=10 \
-          color="$bar_color" \
-        --default \
-          cache_scripts=on \
-          icon.font="$icon_font" \
-          icon.color="$icon_color" \
-          icon.highlight_color="$icon_highlight_color" \
-          label.font="$label_font" \
-          label.color="$label_color" \
-          label.highlight_color="$label_highlight_color" \
-          icon.padding_left=10 \
-          icon.padding_right=6 \
-        --add item clock right \
-        --set clock update_freq=10 script="${scripts}/status.sh" icon.padding_left=2 \
-        --add item battery right \
-        --set battery update_freq=60 script="${scripts}/battery.sh" \
-        --add item wifi right \
-        \ #--set wifi click_script="${scripts}/click-wifi.sh" \
-        --add item load right \
-        --set load icon="􀍽" script="${scripts}/window-indicator.sh" \
-        --subscribe load space_change \
-        --add item network right \
-        --default \
-          icon.padding_left=0 \
-          icon.padding_right=2 \
-          label.padding_right=16 \
-        "''${spaces[@]}"
+          --add item app_name left \
+          --set app_name \
+              label.font="$heavy_font" \
+              label.color="$label_highlight_color" \
+          --add item window left \
+          --set window \
+              script="${scripts}/window-title.sh" \
+          --subscribe window window_focus front_app_switched space_change title_change \
+          --add bracket app_window \
+              app_name window \
+          --set app_window \
+              background.drawing=off
 
-      # EVENTS
-      sketchybar -m --add event window_focus \
-                  --add event title_change
+      #        --subscribe title window_focus front_app_switched space_change title_change
 
-      # WINDOW TITLE 
-      sketchybar -m --add item title left \
-                  --set title script="${scripts}/window-title.sh" \
-                  --subscribe title window_focus front_app_switched space_change title_change
+            sketchybar -m --update
 
-      sketchybar -m --update
-
-      # ram disk
-      cache="$HOME/.cache/sketchybar"
-      mkdir -p "$cache"
-      if ! mount | grep -qF "$cache"
-      then
-        disk=$(hdiutil attach -nobrowse -nomount ram://1024)
-        disk="''${disk%% *}"
-        newfs_hfs -v sketchybar "$disk"
-        mount -t hfs -o nobrowse "$disk" "$cache"
-      fi
+            # ram disk
+            cache="$HOME/.cache/sketchybar"
+            mkdir -p "$cache"
+            if ! mount | grep -qF "$cache"
+            then
+              disk=$(hdiutil attach -nobrowse -nomount ram://1024)
+              disk="''${disk%% *}"
+              newfs_hfs -v sketchybar "$disk"
+              mount -t hfs -o nobrowse "$disk" "$cache"
+            fi
     '';
 
     services.yabai.config = {
--- darwin/modules/sketchybar/window-title.sh
+++ darwin/modules/sketchybar/window-title.sh
@@ -1,11 +1,50 @@
-#!/bin/bash
+#!/bin/zsh
 
-WINDOW_TITLE=$(yabai -m query --windows --window | jq -r '.title')
-WINDOW_APP=$(yabai -m query --windows --window | jq -r '.app')
-LABEL="$WINDOW_APP - $WINDOW_TITLE"
+# window title
+data=$(yabai -m query --windows --window)
 
-if [[ ${#LABEL} -gt 50 ]]; then
-  LABEL="$(echo "$LABEL" | cut -n -b 1-50)…"
+window_title=$(echo $data | jq -r '.title' \
+    | sed 's/ - Eric (Person [[:digit:]])$//g' \
+    | sed 's/ - Google Chrome$//g' \
+    | sed 's/ - Audio playing$//g' \
+    | sed 's/ - Camera or microphone recording$//g' \
+    | sed 's/ - Part of group ..*$//g')
+
+[ "${#window_title}" -gt 40 ] && window_title="$(echo $window_title | head -c 40)…"
+
+# window properties
+
+floating=$(echo $data | jq -r '.floating')
+topmost=$(echo $data | jq -r '.topmost')
+sticky=$(echo $data | jq -r '.sticky')
+zoom_fullscreen=$(echo $data | jq -r '."zoom-fullscreen"')
+zoom_parent=$(echo $data | jq -r '."zoom-parent"')
+
+[ "$floating" = "1" ] && properties="${properties}~"
+[ "$topmost" = "1" ] && properties="${properties}^"
+[ "$zoom_fullscreen" = "1" ] && properties="${properties}f"
+[ "$zoom_parent" = "1" ] && properties="${properties}p"
+[ "$sticky" = "1" ] && properties="${properties}*"
+
+if ! [ -z "$properties" ]; then
+    [ -z "$window_title" ] && window_title="[$properties]" || window_title="$window_title [$properties]"
 fi
 
-sketchybar -m --set title label="│ $LABEL"
+# app name
+
+app_name=$(echo $data | jq -r '.app')
+
+[ "${#app_name}" -gt 20 ] && app_name="$(echo $app_name | head -c 20)…"
+
+# setting items
+
+echo app "$app_name"
+echo title "$window_title"
+
+sketchybar -m \
+    --set app_name \
+        label="$app_name" \
+        drawing=$([ -z "$app_name" ] && echo off || echo on) \
+    --set window \
+        label="$window_title" \
+        drawing=$([ -z "$window_title" ] && echo off || echo on)
