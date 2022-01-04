b85af0610ee06a9dbab0aa81d33d0a9a7abe1627
Thomas Christensen <thomas.christensen@schibsted.com>
2022-01-04 15:16:36 +0100

    refactor skhd-scripts to be in nix-store

--- darwin/modules/skhd.nix
+++ darwin/modules/skhd.nix
@@ -4,6 +4,7 @@ with lib;
 let
   cfg = config.tc.skhd;
   mkAppShortcut = (shortcut: app: "${shortcut} : open -a \"${app}\"");
+  scripts = ./skhd;
 in
 {
   options.tc.skhd = {
@@ -32,18 +33,54 @@ in
       jq
       terminal-notifier
     ];
-    environment.etc."skhd-moveWindowToDisplayAndFollowFocus.sh".source = ./skhd/moveWindowToDisplayAndFollowFocus.sh;
-    environment.etc."skhd-moveWindowToSpaceOnSameDisplay.sh".source = ./skhd/moveWindowToSpaceOnSameDisplay.sh;
-    environment.etc."skhd-toggleLayoutOnCurrentSpace.sh".source = ./skhd/toggleLayoutOnCurrentSpace.sh;
-    environment.etc."skhd-focusFirstWindowInSpace.sh".source = ./skhd/focusFirstWindowInSpace.sh;
-    environment.etc."skhd-moveWindowToFirstEmptySpaceOnSameDisplay.sh".source = ./skhd/moveWindowToFirstEmptySpaceOnSameDisplay.sh;
 
     services.skhd = {
       enable = true;
       # https://github.com/koekeishiya/skhd/issues/1
       skhdConfig =
-        builtins.readFile ./skhd/skhdrc
-        + ''
+        ''
+          hyper - q : ${scripts}/moveWindowToDisplayAndFollowFocus.sh left
+          hyper - w : ${scripts}/moveWindowToDisplayAndFollowFocus.sh right
+          hyper - a : yabai -m display --focus prev || yabai -m display --focus last
+          hyper - s : yabai -m display --focus next || yabai -m display --focus first
+
+          hyper - o : ${scripts}/moveWindowToSpaceOnSameDisplay.sh prev
+          hyper - p : ${scripts}/moveWindowToSpaceOnSameDisplay.sh next
+          hyper - tab : ${scripts}/moveWindowToFirstEmptySpaceOnSameDisplay.sh
+
+          hyper - return : ${scripts}/toggleLayoutOnCurrentSpace.sh
+
+          hyper - d : yabai -m space --toggle show-desktop
+          hyper - e : yabai -m space --toggle mission-control
+
+          hyper - 1 : ${scripts}/focusFirstWindowInSpace.sh 1
+          hyper - 2 : /${scripts}/focusFirstWindowInSpace.sh 2
+          hyper - 3 : ${scripts}/focusFirstWindowInSpace.sh 3
+          hyper - 4 : ${scripts}/focusFirstWindowInSpace.sh 4
+          hyper - 5 : ${scripts}/focusFirstWindowInSpace.sh 5
+          hyper - 6 : ${scripts}/focusFirstWindowInSpace.sh 6
+          hyper - 7 : ${scripts}/focusFirstWindowInSpace.sh 7
+          hyper - 8 : ${scripts}/focusFirstWindowInSpace.sh 8
+
+          hyper - j : yabai -m window --focus west
+          hyper - l : yabai -m window --focus east
+          hyper - i : yabai -m window --focus north
+          hyper - k : yabai -m window --focus south
+
+          hyper - m : yabai -m window --toggle native-fullscreen
+          hyper - f : yabai -m window --toggle zoom-fullscreen
+
+          hyper - left  : yabai -m window --resize left:-20:0; \
+                          yabai -m window --resize right:-20:0;
+          hyper - right : yabai -m window --resize right:20:0; \
+                          yabai -m window --resize left:20:0;
+          hyper - down  : yabai -m window --resize top:0:20; \
+                          yabai -m window --resize bottom:0:20
+          hyper - up    : yabai -m window --resize bottom:0:-20; \
+                          yabai -m window --resize top:0:-20
+
+          hyper - space : yabai -m space --rotate 270
+
           # app shortcuts
           hyper - b : open -a "${cfg.browser}"
           hyper - t : open -a "iTerm"
