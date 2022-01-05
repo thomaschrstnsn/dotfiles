71c809c86fd4c51bdaaef3b986bcdeb12ede4b1f
Thomas Christensen <thomas@chrstnsn.dk>
2022-01-05 22:44:09 +0100

    remove unused/unneeded

--- darwin/modules/sketchybar/window-title.sh
+++ darwin/modules/sketchybar/window-title.sh
@@ -12,24 +12,6 @@ window_title=$(echo $data | jq -r '.title' \
 
 [ "${#window_title}" -gt 40 ] && window_title="$(echo $window_title | head -c 40)…"
 
-# window properties
-
-floating=$(echo $data | jq -r '.floating')
-topmost=$(echo $data | jq -r '.topmost')
-sticky=$(echo $data | jq -r '.sticky')
-zoom_fullscreen=$(echo $data | jq -r '."zoom-fullscreen"')
-zoom_parent=$(echo $data | jq -r '."zoom-parent"')
-
-[ "$floating" = "1" ] && properties="${properties}~"
-[ "$topmost" = "1" ] && properties="${properties}^"
-[ "$zoom_fullscreen" = "1" ] && properties="${properties}f"
-[ "$zoom_parent" = "1" ] && properties="${properties}p"
-[ "$sticky" = "1" ] && properties="${properties}*"
-
-if ! [ -z "$properties" ]; then
-    [ -z "$window_title" ] && window_title="[$properties]" || window_title="$window_title [$properties]"
-fi
-
 # app name
 
 app_name=$(echo $data | jq -r '.app')
