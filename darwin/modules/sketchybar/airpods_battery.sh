64a96e352841383d00958cc3366193cc496fb799
Thomas Christensen <thomas.christensen@schibsted.com>
2022-06-14 08:42:59 +0200

    fix airpod battery script

--- darwin/modules/sketchybar/airpods_battery.sh
+++ darwin/modules/sketchybar/airpods_battery.sh
@@ -4,14 +4,12 @@ fi
 
 DEVICE_ADDRESS="AC:1D:06:0B:7A:6B"
 
-DEVICE=$(system_profiler SPBluetoothDataType -json -detailLevel basic 2>/dev/null | jq '.SPBluetoothDataType' | jq '.[0]' | jq '.devices_list' | jq -r ".[] | select(.[] .device_address==\"$DEVICE_ADDRESS\") | .[]")
+DEVICE_CONNECTED=$(system_profiler SPBluetoothDataType -json -detailLevel basic 2>/dev/null | jq '.SPBluetoothDataType' | jq '.[0]' | jq '.device_connected' | jq -r ".[] | select(.[] .device_address==\"$DEVICE_ADDRESS\") | .[]")
 
-CONNECTED="$(echo "$DEVICE" | jq -r '.device_connected=="Yes"')"
-
-if [ "$CONNECTED" = "true" ]; then
-  LEFT=$( echo "$DEVICE" | jq -r '.device_batteryLevelLeft'  | sed 's/[^0-9]//g')
-  RIGHT=$(echo "$DEVICE" | jq -r '.device_batteryLevelRight' | sed 's/[^0-9]//g')
-  CASE=$( echo "$DEVICE" | jq -r '.device_batteryLevelCase'  | sed 's/[^0-9]//g')
+if [[ -n "$DEVICE_CONNECTED" ]] ; then
+  LEFT=$( echo "$DEVICE_CONNECTED" | jq -r '.device_batteryLevelLeft'  | sed 's/[^0-9]//g')
+  RIGHT=$(echo "$DEVICE_CONNECTED" | jq -r '.device_batteryLevelRight' | sed 's/[^0-9]//g')
+  CASE=$( echo "$DEVICE_CONNECTED" | jq -r '.device_batteryLevelCase'  | sed 's/[^0-9]//g')
 
   WARNING_LEVEL=25
   if [ $LEFT -le $WARNING_LEVEL ] || [ $RIGHT -le $WARNING_LEVEL ] || [ $CASE -le $WARNING_LEVEL ]; then
