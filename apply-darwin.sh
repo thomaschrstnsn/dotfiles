8d52c5af799c5b977d5a2f44a4f6644621f1911c
Thomas Christensen <thomas.christensen@schibsted.com>
2022-07-26 06:04:00 +0200

    args to apply-darwin

--- apply-darwin.sh
+++ apply-darwin.sh
@@ -1,10 +1,16 @@
-#! /bin/sh
+#! /usr/bin/env bash
 set -e
 
 ./build-darwin.sh "$*"
 
 DEFAULT_CONFIGURATION="$(hostname -s)"
-CONFIGURATION="${1:-$DEFAULT_CONFIGURATION}"
+if [[ $1 == "--"* ]] || [[ $# -eq 0 ]];
+then
+    CONFIGURATION=$DEFAULT_CONFIGURATION
+else
+    CONFIGURATION="${1:-$DEFAULT_CONFIGURATION}"
+    shift
+fi
 
 echo activating darwin "$CONFIGURATION"
-./result/sw/bin/darwin-rebuild switch --flake .\#"$CONFIGURATION"
+./result/sw/bin/darwin-rebuild switch --flake .\#"$CONFIGURATION" "$@"
