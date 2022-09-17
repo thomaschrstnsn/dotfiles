e871e4c7597e7c2a2b158463a243490d6460f23b
Thomas Christensen <thomas@chrstnsn.dk>
2022-09-17 20:12:13 +0200

    homebrew changes in darwin

--- darwin/modules/homebrew.nix
+++ darwin/modules/homebrew.nix
@@ -33,10 +33,10 @@ in
     '';
 
     homebrew.enable = true;
-    homebrew.autoUpdate = true;
-    homebrew.cleanup = "zap";
+    homebrew.onActivation.autoUpdate = true;
+    homebrew.onActivation.cleanup = "zap";
     homebrew.global.brewfile = true;
-    homebrew.global.noLock = true;
+    homebrew.global.lockfiles = true;
 
     homebrew.masApps = {
       # Xcode = 497799835;
