4429eced827c90b4a7a5767df0b05b33b4c49c1c
Thomas Christensen <thomas@chrstnsn.dk>
2022-09-25 20:21:36 +0200

    add aero-nix initial
    fix
    
    from installer
    
    brave

--- machines.nix
+++ machines.nix
@@ -197,6 +197,58 @@ in
       system = systems.x64-darwin;
     };
 
+    aero-nix =
+      let
+        username = "thomas";
+        configurationRoot = ./nixos/machines/aero-nix;
+      in
+      {
+        home = {
+          user = {
+            username = username;
+            homedir = "/home/${username}";
+          };
+          dotnet = {
+            enable = true;
+            sdks = [ "6.0" ];
+          };
+          git.enable = true;
+          ssh = {
+            enable = true;
+            hosts = [ "rpi4" "vmnix" ];
+          };
+          zsh = {
+            enable = true;
+          };
+        };
+
+        extraPackages = pkgs: with pkgs; [
+          brave
+          shellcheck
+          rnix-lsp
+          nixpkgs-fmt
+          _1password-gui
+          vscode
+        ];
+
+        nixos = {
+          config = {
+            user = {
+              name = username;
+              groups = [ "wheel" ];
+            };
+            networking.hostname = "aero-nix";
+          };
+          base = {
+            imports = [
+              (configurationRoot + "/configuration.nix")
+            ];
+          };
+        };
+
+        system = systems.x64-linux;
+      };
+
     "${DESKTOP-IP1G00V}" =
       let
         username = "nixos";
