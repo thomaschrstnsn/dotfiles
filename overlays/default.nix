724d3fa9a73a2f8b996e983f8a8e8ec42be90610
Thomas Christensen <thomas.christensen@schibsted.com>
2022-12-20 13:59:30 +0100

    remove overlay for yabai (use in nixpkgs version)

--- overlays/default.nix
+++ overlays/default.nix
@@ -21,33 +21,6 @@
             };
           }
         );
-
-      # https://github.com/azuwis/nix-config/blob/f927c463e42c7bf017ddf4a603c0fcd282bf1d98/darwin/overlays.nix#L72-L93
-      yabai =
-        let
-          replace = {
-            "aarch64-darwin" = "--replace '-arch x86_64' ''";
-            "x86_64-darwin" = "--replace '-arch arm64e' '' --replace '-arch arm64' ''";
-          }.${super.pkgs.stdenv.hostPlatform.system};
-        in
-        super.yabai.overrideAttrs (
-          o: rec {
-            version = "4.0.1";
-            src = super.fetchFromGitHub {
-              owner = "koekeishiya";
-              repo = "yabai";
-              rev = "v${version}";
-              sha256 = "sha256-H1zMg+/VYaijuSDUpO6RAs/KLAAZNxhkfIC6CHk/xoI=";
-            };
-            prePatch = ''
-              substituteInPlace makefile ${replace}
-              substituteInPlace src/workspace.m --replace 'screen.safeAreaInsets.top' '0'
-            '';
-            buildPhase = ''
-              PATH=/usr/bin:/bin /usr/bin/make install #yo
-            '';
-          }
-        );
     })
   ];
 }
