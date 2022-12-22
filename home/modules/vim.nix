7e13f793904d9a62123b60cf0ddbc765ceed9193
Thomas Christensen <thomas.christensen@schibsted.com>
2022-12-22 13:43:15 +0100

    adjust the bar char for block highlight

--- home/modules/vim.nix
+++ home/modules/vim.nix
@@ -319,7 +319,7 @@ in
             space_char_blankline = " ",
             show_current_context = true,
             show_current_context_start = true,
-            char = '┊',
+            char = '│',
             show_trailing_blankline_indent = false,
         }
       '';
