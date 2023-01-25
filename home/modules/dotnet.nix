e3cfaa799decd5516c4df491f040534c4b503acc
Thomas Christensen <thomas.christensen@schibsted.com>
2023-01-25 13:44:46 +0100

    rider alias not needed when using toolbox

--- home/modules/dotnet.nix
+++ home/modules/dotnet.nix
@@ -37,7 +37,7 @@ in
     programs.git.ignores = [ "consul-settings-backup.json" ];
 
     programs.zsh.shellAliases = {
-      rider = "open -a Rider";
+      # rider = "open -a Rider"; # there should be script in in /usr/local/bin/rider from the toolbox: https://www.jetbrains.com/help/rider/Working_with_the_IDE_Features_from_Command_Line.html#10c968a9
       r = "rider $(fd --type f --glob '*.{sln,??proj}' | fzf)";
     };
 
