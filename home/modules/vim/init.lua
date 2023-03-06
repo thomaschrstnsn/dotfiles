feab82ca2fe33b41c9e413c7a2e81111e3b13839
Thomas Christensen <thomas@chrstnsn.dk>
2023-03-06 21:34:24 +0100

    barbar play nice with nvim-tree

--- home/modules/vim/init.lua
+++ home/modules/vim/init.lua
@@ -145,3 +145,23 @@ require("indent_blankline").setup {
     char = '│',
     show_trailing_blankline_indent = false
 }
+
+-- barbar play nice with nvim-tree
+local nvim_tree_events = require('nvim-tree.events')
+local bufferline_api = require('bufferline.api')
+
+local function get_tree_size()
+  return require'nvim-tree.view'.View.width
+end
+
+nvim_tree_events.subscribe('TreeOpen', function()
+  bufferline_api.set_offset(get_tree_size())
+end)
+
+nvim_tree_events.subscribe('Resize', function()
+  bufferline_api.set_offset(get_tree_size())
+end)
+
+nvim_tree_events.subscribe('TreeClose', function()
+  bufferline_api.set_offset(0)
+end)
