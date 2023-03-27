2767309da69d9ec4fe83a7e9985307d25f2c62aa
Thomas Christensen <thomas.christensen@schibsted.com>
2023-03-27 08:33:36 +0200

    adjust sizing of symbols on wezterm

--- home/modules/wezterm/wezterm.lua
+++ home/modules/wezterm/wezterm.lua
@@ -4,6 +4,10 @@ local act = wezterm.action
 return {
   send_composed_key_when_left_alt_is_pressed = true,
   color_scheme = 'tokyonight',
+  font = wezterm.font_with_fallback({
+	  "JetBrains Mono",
+	  { family = "Symbols Nerd Font Mono", scale = 0.8 },
+  }),
   font_size = 15,
   window_decorations = 'RESIZE',
   hide_tab_bar_if_only_one_tab = true,
