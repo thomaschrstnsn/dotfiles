c609df9047cd169998867655a981db166355231a
Thomas Christensen <thomas@chrstnsn.dk>
2023-04-20 12:03:17 +0000

    dcnix: add initial setup

---
+++ machines/wsl/dcnix.nix
@@ -0,0 +1,44 @@
+{ systems, ... }:
+
+let
+  username = "nixos";
+in
+{
+  home = {
+    user = {
+      username = username;
+      homedir = "/home/${username}";
+    };
+    git = {
+      enable = true;
+      githubs = [ ];
+    };
+    ssh = {
+      enable = true;
+      hosts = [ "rpi4" ];
+    };
+    wsl.enable = true;
+    zsh = {
+      enable = true;
+      editor = "nvim";
+    };
+    vim.enable = true;
+    direnv.enable = true;
+  };
+  system = systems.x64-linux;
+
+  nixos = {
+    config = {
+      user = {
+        name = username;
+        groups = [ "wheel" ];
+      };
+      networking.hostname = "PC04137";
+    };
+    base = {
+      imports = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
