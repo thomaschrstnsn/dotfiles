8f3c106327734af148cdccead0fdb20aa7ca4c4c
Thomas Christensen <thomas@chrstnsn.dk>
2023-05-30 20:22:22 +0200

    yabai: use nix tree version (but newer)
    Solves the focus on other screen issue present until 5.0.2 atleast
    
    https://github.com/koekeishiya/yabai/issues/1764

--- overlays/default.nix
+++ overlays/default.nix
@@ -22,8 +22,17 @@
           }
         );
 
-      # https://github.com/azuwis/nix-config/tree/c38647695bd6180a2037dfa1675d5a9322250a3c/pkgs/yabai
-      yabai = super.callPackage ./yabai { };
-    })
+      yabai = super.yabai.overrideAttrs
+        (
+          o: rec {
+            version = "5.0.6";
+            src = super.fetchzip {
+              url = "https://github.com/koekeishiya/yabai/releases/download/v${version}/yabai-v${version}.tar.gz";
+              sha256 = "sha256-wpm9VnR4yPk6Ybo/V2DMLgRcSzDl3dWGSKDCjYfz+xQ";
+            };
+          }
+        );
+    }
+    )
   ];
 }
