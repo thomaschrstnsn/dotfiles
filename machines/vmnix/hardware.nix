1c34e34af39074c1795346272c4d29fc60998207
Thomas Christensen <thomas@chrstnsn.dk>
2023-07-05 22:37:42 +0200

    vmnix: new vm

--- machines/vmnix/hardware.nix
+++ machines/vmnix/hardware.nix
@@ -14,17 +14,25 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/db099679-1a4d-4a11-8de4-06abba1074c2";
+    { device = "/dev/disk/by-uuid/c0eeb77c-100f-4509-bbea-877494f1cee4";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/9635-5121";
+    { device = "/dev/disk/by-uuid/FE9E-2528";
       fsType = "vfat";
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/8f9fef07-03b6-4d73-b681-2770e22a2235"; }
+    [ { device = "/dev/disk/by-uuid/8b2cc979-e54b-4f2f-884b-8944419520fa"; }
     ];
 
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp0s6.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
 }
