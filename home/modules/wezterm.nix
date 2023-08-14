7774c1c9d36b49e9bb1120bba90b7ba6e58653ff
Thomas Christensen <thomas@chrstnsn.dk>
2023-08-14 16:12:10 +0200

    wezterm: no audible bell

--- home/modules/wezterm.nix
+++ home/modules/wezterm.nix
@@ -18,7 +18,7 @@ in
     home.packages = with pkgs; [ wezterm ];
     home.file = {
       ".config/wezterm/wezterm.lua".text = replaceStrings
-        [ "##FONTSIZE##" ]
+        [ ''"FONTSIZE"'' ]
         [ (toString cfg.fontsize) ]
         (readFile ./wezterm/wezterm.lua);
     };
--- home/modules/wezterm/wezterm.lua
+++ home/modules/wezterm/wezterm.lua
@@ -1,13 +1,20 @@
 local wezterm = require 'wezterm'
 
 return {
-  send_composed_key_when_left_alt_is_pressed = true,
-  color_scheme = 'tokyonight',
-  font = wezterm.font_with_fallback({
-	  "JetBrains Mono",
-	  { family = "Symbols Nerd Font Mono", scale = 0.8 },
-  }),
-  font_size = ##FONTSIZE##,
-  window_decorations = 'RESIZE',
-  hide_tab_bar_if_only_one_tab = true,
+	send_composed_key_when_left_alt_is_pressed = true,
+	color_scheme = 'tokyonight',
+	font = wezterm.font_with_fallback({
+		"JetBrains Mono",
+		{ family = "Symbols Nerd Font Mono", scale = 0.8 },
+	}),
+	font_size = "FONTSIZE",
+	window_decorations = 'RESIZE',
+	hide_tab_bar_if_only_one_tab = true,
+	visual_bell = {
+		fade_in_function = 'EaseIn',
+		fade_in_duration_ms = 150,
+		fade_out_function = 'EaseOut',
+		fade_out_duration_ms = 150,
+	},
+	audible_bell = 'Disabled',
 }
