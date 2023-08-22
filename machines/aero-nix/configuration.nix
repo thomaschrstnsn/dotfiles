5c86476266f9c36c632f22302f257071d11ea68c
Thomas Christensen <thomas@chrstnsn.dk>
2023-08-22 17:46:56 +0200

    aero/nixos: limit to 10 configurations in /boot
    ran out of space :(

--- machines/aero-nix/configuration.nix
+++ machines/aero-nix/configuration.nix
@@ -15,6 +15,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.systemd-boot.configurationLimit = 10;
 
   networking.hostName = "aero-nix"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
