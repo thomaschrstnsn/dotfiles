454627ee1f56a9676a5e938b8d6553c4b7ef8128
Thomas Christensen <thomas@chrstnsn.dk>
2023-08-29 22:15:58 +0200

    aero: greetd + tuigreet

--- machines/aero-nix/configuration.nix
+++ machines/aero-nix/configuration.nix
@@ -27,6 +27,14 @@
     libinput.enable = true;
   };
 
+  services.greetd = {
+    enable = true;
+    settings.default_session = {
+      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+      user = "greeter";
+    };
+  };
+
   console = {
     keyMap = "uk";
     font = "ter-i20n";
