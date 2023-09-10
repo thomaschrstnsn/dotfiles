ea33d0c724b75b87c0ea9548807d440a15c9dfbe
Thomas Christensen <thomas@chrstnsn.dk>
2023-09-10 12:04:57 +0200

    vim: add harpoon

--- home/modules/vim.nix
+++ home/modules/vim.nix
@@ -139,6 +139,21 @@ in
         barbar = { enable = true; };
         comment-nvim = { enable = true; };
         gitgutter.enable = true;
+        harpoon = {
+          enable = true;
+          keymaps = {
+            addFile = "<leader>ja";
+            toggleQuickMenu = "<leader>jj";
+            navFile = {
+              "1" = "<C-f>";
+              "2" = "<C-d>";
+              "3" = "<C-s>";
+              "4" = "<C-a>";
+            };
+            navNext = "<C-]>";
+            navPrev = "<C-[>";
+          };
+        };
         indent-blankline = {
           enable = true;
           showCurrentContext = true;
