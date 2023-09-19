0f74e0497378a0786a61c8f83b703c2015bc0b4e
Thomas Christensen <thomas@chrstnsn.dk>
2023-09-19 22:46:47 +0200

    macos: remove custom yabai pkg

--- overlays/default.nix
+++ overlays/default.nix
@@ -8,18 +8,6 @@
       gum = myPkgs.gum;
 
       myNerdfonts = self.nerdfonts.override { fonts = [ "JetBrainsMono" "Meslo" ]; };
-
-      yabai = super.yabai.overrideAttrs
-        (
-          o: rec {
-            version = "5.0.6";
-            src = super.fetchzip {
-              url = "https://github.com/koekeishiya/yabai/releases/download/v${version}/yabai-v${version}.tar.gz";
-              sha256 = "sha256-wpm9VnR4yPk6Ybo/V2DMLgRcSzDl3dWGSKDCjYfz+xQ";
-            };
-          }
-        );
-    }
-    )
+    })
   ];
 }
