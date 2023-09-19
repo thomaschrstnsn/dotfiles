9fa103be6503a20373e03d2de2d93f694b917b01
Thomas Christensen <thomas@chrstnsn.dk>
2023-09-19 22:45:56 +0200

    macos: remove custom sketchybar pkg

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,19 +9,6 @@
 
       myNerdfonts = self.nerdfonts.override { fonts = [ "JetBrainsMono" "Meslo" ]; };
 
-      sketchybar =
-        super.sketchybar.overrideAttrs (
-          o: rec {
-            version = "2.15.1";
-            src = super.fetchFromGitHub {
-              owner = "FelixKratz";
-              repo = "SketchyBar";
-              rev = "v${version}";
-              sha256 = "sha256-0jCVDaFc7ZvA8apeHRoQvPhAlaGlBHzqUkS9or88PcM";
-            };
-          }
-        );
-
       yabai = super.yabai.overrideAttrs
         (
           o: rec {
