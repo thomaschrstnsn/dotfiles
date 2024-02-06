4d708ddf2b611bf6514a679a43860e8bb81b11ab
Thomas Christensen <thomas@chrstnsn.dk>
2024-02-06 13:29:31 +0100

    rpi4: enable swap on ssd

--- machines/nixos-raspi-4/configuration.nix
+++ machines/nixos-raspi-4/configuration.nix
@@ -81,5 +81,5 @@
     allowUnfree = true;
   };
   system.stateVersion = "20.09";
-  #swapDevices = [ { device = "/swapfile"; size = 3072; } ];
+  swapDevices = [{ device = "/dev/disk/by-label/swap"; size = 4096; }];
 }
