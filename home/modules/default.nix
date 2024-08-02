6a17e96ac34f27855a3ef7d075a6916da08d449f
Thomas Christensen <thomas@chrstnsn.dk>
2024-08-02 08:07:41 +0200

    nix/shell: realise-symlink

--- home/modules/default.nix
+++ home/modules/default.nix
@@ -9,14 +9,15 @@
     ./home.nix
     ./nodejs.nix
     ./rancher.nix
+    ./realise_symlink.nix
     ./sesh.nix
     ./smd_launcher.nix
     ./ssh.nix
     ./sway.nix
     ./tmux.nix
     ./vim.nix
-    ./wsl.nix
     ./wezterm.nix
+    ./wsl.nix
     ./zsh.nix
 
     ./haskell
