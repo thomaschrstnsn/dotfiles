5613b5a587598a51f91d98eb72ecaccc1be84970
Thomas Christensen <thomas@chrstnsn.dk>
2024-08-02 08:22:43 +0200

    fonts: include 'nerdfont symbols only'

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,7 +5,7 @@
     (self: super: {
       inherit myPkgs;
 
-      myNerdfonts = self.nerdfonts.override { fonts = [ "JetBrainsMono" "Meslo" ]; };
+      myNerdfonts = self.nerdfonts.override { fonts = [ "JetBrainsMono" "Meslo" "NerdFontsSymbolsOnly" ]; };
 
       # https://github.com/DieracDelta/vimconfig/blob/ec8062dfe4ce0776fea5e42c28e61fe946ce6c27/plugins.nix#L135
       code-lldb = lldb-nix-fix.legacyPackages.${super.system}.vscode-extensions.vadimcn.vscode-lldb;
