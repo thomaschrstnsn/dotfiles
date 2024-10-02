433d71311656ecb366ebfc604ba52c43615e5082
Thomas Christensen <thomas@chrstnsn.dk>
2024-10-02 22:06:49 +0200

    skhd+aerospace: use hyperkeys for some (partial skhd/aerospace toml)

--- darwin/modules/aerospace.nix
+++ darwin/modules/aerospace.nix
@@ -4,11 +4,14 @@ with lib;
 
 let
   cfg = config.tc.aerospace;
+
+  toSkhdConfig = attrs: concatLines (mapAttrsToList (key: cmd: "${key}: /opt/homebrew/bin/aerospace ${cmd}") attrs);
 in
 {
-  options.tc.aerospace = with types; {
-    enable = mkEnableOption "aerospace tiling window manager https://github.com/nikitabobko/AeroSpace";
-  };
+  options.tc.aerospace = with types;
+    {
+      enable = mkEnableOption "aerospace tiling window manager https://github.com/nikitabobko/AeroSpace";
+    };
   config = mkIf cfg.enable {
     system.defaults.NSGlobalDomain._HIHideMenuBar = true;
 
@@ -20,6 +23,17 @@ in
     # https://nikitabobko.github.io/AeroSpace/guide#a-note-on-mission-control
     system.defaults.dock.expose-group-by-app = true;
 
-    services.skhd.skhdConfig = '''';
+    services.skhd.skhdConfig = toSkhdConfig {
+      "hyper - q" = "move-workspace-to-monitor --wrap-around prev";
+      "hyper - w" = "move-workspace-to-monitor --wrap-around next";
+      "hyper - f" = "fullscreen";
+      "hyper - h" = "focus left";
+      "hyper - j" = "focus down";
+      "hyper - k" = "focus up";
+      "hyper - l" = "focus right";
+      "hyper - left" = "resize smart -50";
+      "hyper - right" = "resize smart +50";
+      "hyper - escape" = "reload-config";
+    };
   };
 }
