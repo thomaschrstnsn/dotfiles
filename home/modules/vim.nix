55e527d72a8d77361fd0ad8a8681d4c9de225425
Thomas Christensen <thomas@chrstnsn.dk>
2024-10-05 00:01:51 +0200

    vim: try a cmp setup that does not interfere with normal typing

--- home/modules/vim.nix
+++ home/modules/vim.nix
@@ -163,25 +163,10 @@ in
         cmp = {
           enable = true;
           settings.mapping = {
-            "<CR>" = "cmp.mapping.confirm({ select = true })";
-            "<Tab>" = ''cmp.mapping(function(fallback)
-                -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
-                if (cmp.visible()) then
-                  local entry = cmp.get_selected_entry()
-                  if not entry then
-                    cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-                  else
-                    cmp.confirm()
-                  end
-                else
-                  fallback()
-                end
-              end, {"i","s",}) '';
-            "<C-p>" = "cmp.mapping.select_prev_item()";
-            "<C-n>" = "cmp.mapping.select_next_item()";
-            "<Up>" = "cmp.mapping.select_prev_item()";
-            "<Down>" = "cmp.mapping.select_next_item()";
-            "<C-Space>" = "cmp.mapping.complete()";
+            "<C-y>" = "cmp.mapping.confirm({ select = true })";
+            "<C-CR>" = "cmp.mapping.confirm({ select = true })";
+            "<C-p>" = "cmp.mapping.select_prev_item { behavior == cmp.SelectBehavior.Insert }";
+            "<C-n>" = "cmp.mapping.select_next_item { behavior == cmp.SelectBehavior.Insert }";
           };
 
           settings.snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
