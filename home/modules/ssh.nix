45253d992ae665c33716b3d4808a9435a21ea585
Thomas Christensen <thomas@chrstnsn.dk>
2024-10-23 11:18:42 +0200

    export SSH_AUTH_SOCK for 1password agent (makes jj libgit2 integration work)

--- home/modules/ssh.nix
+++ home/modules/ssh.nix
@@ -141,6 +141,9 @@ in
       }
       (mkIf cfg.use1PasswordAgentOnMac
         {
+          programs.zsh.initExtraBeforeCompInit = ''
+            export SSH_AUTH_SOCK="/Users/$USER/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+          '';
           home.file = {
             ".config/1Password/ssh/agent.toml".text = ''
               [[ssh-keys]]
