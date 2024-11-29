260f12dad3480b3ec69d6599a4175997a27cc6be
Thomas Christensen <thomas@chrstnsn.dk>
2024-11-30 00:59:31 +0100

    hyprland: update config to match version

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -269,10 +269,12 @@ in
             active_opacity = 1.0;
             inactive_opacity = 1.0;
 
-            drop_shadow = true;
-            shadow_range = 4;
-            shadow_render_power = 3;
-            "col.shadow" = "rgba(1a1a1aee)";
+            shadow = {
+              enabled = true;
+              color = "rgba(1a1a1aee)";
+              range = 4;
+              render_power = 3;
+            };
 
             blur = {
               enabled = true;
