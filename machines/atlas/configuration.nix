cb168762b8e474ea55155c2fb2d8265e34d4b32c
Thomas Christensen <thomas@chrstnsn.dk>
2025-01-04 00:06:31 +0100

    atlas: gnome polkit

--- machines/atlas/configuration.nix
+++ machines/atlas/configuration.nix
@@ -113,6 +113,22 @@ in
   security.polkit.enable = true;
   security.pam.services.hyprlock = { };
 
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+
   services.pipewire = {
     enable = true;
     alsa.enable = true;
