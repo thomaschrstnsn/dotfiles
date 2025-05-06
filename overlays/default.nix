5a51db844c564e12b59640809a7850337f72e456
Thomas Christensen <thomas@chrstnsn.dk>
2025-05-06 16:02:07 +0200

    lnav: bump down one version

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,7 +5,35 @@
     hyprpanel.overlay
     (self: super: {
       inherit myPkgs;
+    })
 
+    (final: prev: {
+      ## 0.12.4 has an annoying issue with tmux TERM definition
+      lnav = prev.lnav.overrideAttrs (_: rec {
+        version = "0.12.3";
+
+        src = prev.fetchFromGitHub {
+          owner = "tstack";
+          repo = "lnav";
+          rev = "v${version}";
+          sha256 = "sha256-m0r7LAo9pYFpS+oimVCNCipojxPzMMsLLjhjkitEwow=";
+        };
+
+        nativeBuildInputs = with prev.pkgs; [
+          autoconf
+          automake
+          zlib
+          curl.dev
+        ];
+        buildInputs = with prev.pkgs; [
+          bzip2
+          ncurses
+          pcre2
+          readline
+          sqlite
+          curl
+        ];
+      });
     })
   ];
 }
