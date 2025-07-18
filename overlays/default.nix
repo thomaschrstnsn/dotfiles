2e55a054d9cc9e45c734894f46b8fc86eb7f403c
Thomas Christensen <thomas@chrstnsn.dk>
2025-07-18 08:25:44 +0200

    lnav: disable overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,18 +7,19 @@
       inherit myPkgs;
     })
 
-    (final: prev: {
-      ## 0.12.4 has an annoying issue with tmux TERM definition
-      lnav = prev.lnav.overrideAttrs (_: rec {
-        version = "0.13.0-rc4";
-
-        src = prev.fetchFromGitHub {
-          owner = "tstack";
-          repo = "lnav";
-          rev = "v${version}";
-          sha256 = "sha256-Dk+pFg+Tt1krFmEzoT4sW8dPd0x5kCD6vTIQVzAvs3A";
-        };
-      });
-    })
+    # left as an example of how to make an override
+    # (final: prev: {
+    #   ## 0.12.4 has an annoying issue with tmux TERM definition
+    #   lnav = prev.lnav.overrideAttrs (_: rec {
+    #     version = "0.13.0-rc4";
+    #
+    #     src = prev.fetchFromGitHub {
+    #       owner = "tstack";
+    #       repo = "lnav";
+    #       rev = "v${version}";
+    #       sha256 = "sha256-Dk+pFg+Tt1krFmEzoT4sW8dPd0x5kCD6vTIQVzAvs3A";
+    #     };
+    #   });
+    # })
   ];
 }
