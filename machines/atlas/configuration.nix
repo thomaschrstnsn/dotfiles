983ae6a2129bfb2a40c44dbfaa63a69d4f583b1d
Thomas Christensen <thomas@chrstnsn.dk>
2025-07-21 00:42:10 +0200

    atlas: slightly less demanding boot splash

--- machines/atlas/configuration.nix
+++ machines/atlas/configuration.nix
@@ -21,11 +21,11 @@ in
     };
     plymouth = {
       enable = true;
-      theme = "abstract_ring";
+      theme = "rings";
       themePackages = with pkgs; [
         # By default we would install all themes
         (adi1090x-plymouth-themes.override {
-          selected_themes = [ "abstract_ring" ];
+          selected_themes = [ "rings" ];
         })
       ];
     };
