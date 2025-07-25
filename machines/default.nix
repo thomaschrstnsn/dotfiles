ef488ead09ced8792df059151af7d9366db03b57
Thomas Christensen <thomas@chrstnsn.dk>
2025-07-26 01:05:28 +0200

    refactor: rename wsl atlas file, to avoid confusion

--- machines/default.nix
+++ machines/default.nix
@@ -7,7 +7,7 @@
     aeris = import ./aeris { inherit inputs; };
     tilia = import ./lind { inherit inputs; };
     enix = import ./enix/default.nix { inherit inputs; };
-    Atlas = import ./wsl/atlas.nix { inherit inputs; };
+    Atlas = import ./wsl { inherit inputs; };
     nixos-raspi-4 = import ./nixos-raspi-4 { inherit inputs; };
     vmnix = import ./vmnix { inherit inputs; };
   };
