97e7a5ab306120cc245f852a43c7bec739b66b4a
Thomas Christensen <thomas@chrstnsn.dk>
2025-08-21 22:22:16 +0200

    lazyvim: start moving over essentials

---
+++ home/modules/lazy/config/keymaps.lua
@@ -0,0 +1,6 @@
+vim.keymap.del("n", "<leader>wd")
+
+local map = vim.keymap.set
+
+map("n", "<leader>w", "<cmd>w<cr><esc>", { desc = "Save File" } )
+map("n", "-", "<cmd>Oil<cr>", { desc = "Save File" } )
--- home/modules/lazyvim.nix
+++ home/modules/lazyvim.nix
@@ -18,10 +18,31 @@ in
         };
       };
       plugins = with pkgs.vimPlugins; [
+        crates-nvim
+        oil-nvim
+        rose-pine
+        rustaceanvim
         undotree
         vim-tmux-navigator
       ];
-      pluginsFile."editor.lua".source = ./lazy/editor.lua;
+      pluginsFile = {
+        "editor.lua".source = ./lazy/plugins/editor.lua;
+        "oil.lua".source = ./lazy/plugins/oil.lua;
+        "rose-pine.lua".source = ./lazy/plugins/rose-pine.lua;
+        "rust.lua".source = ./lazy/plugins/rust.lua;
+      };
+
+      pluginsToDisable = [
+        # # example - tokyonight seems to be required
+        # {
+        #   lazyName = "tokyonight.nvim";
+        #   nixName = "tokyonight-nvim";
+        # }
+      ];
+    };
+
+    xdg.configFile = {
+      "nvim/lua/config/keymaps.lua".source = ./lazy/config/keymaps.lua;
     };
 
     home.packages = with pkgs; [
@@ -29,5 +50,9 @@ in
       taplo
       nixpkgs-fmt
     ];
+
+    home.shellAliases = {
+      vim = "nvim";
+    };
   };
 }
