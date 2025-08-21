ed68cdc3238e220cfbaa9ea8ec130ba7f2cbe55d
Thomas Christensen <thomas@chrstnsn.dk>
2025-08-21 23:47:38 +0200

    lazy/oil: remember keybinds

--- home/modules/lazy/plugins/oil.lua
+++ home/modules/lazy/plugins/oil.lua
@@ -3,7 +3,15 @@ return {
 		'stevearc/oil.nvim',
 		---@module 'oil'
 		---@type oil.SetupOpts
-		opts = {},
+		opts = {
+			keymaps = {
+				["<C-h>"] = false,
+				["<C-l>"] = false,
+				["q"] = { "actions.close", mode = "n" },
+			},
+			skip_confirm_for_simple_edits = true,
+			delete_to_trash = true,
+		},
 		-- Optional dependencies
 		dependencies = { { "echasnovski/mini.icons", opts = {} } },
 		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
