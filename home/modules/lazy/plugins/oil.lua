479332f8ee1cc79dd2f56b2933af8f9f04e13f53
Thomas Christensen <thomas@chrstnsn.dk>
2025-08-21 23:56:54 +0200

    lazy/oil: show hidden files

--- home/modules/lazy/plugins/oil.lua
+++ home/modules/lazy/plugins/oil.lua
@@ -11,12 +11,15 @@ return {
 			},
 			skip_confirm_for_simple_edits = true,
 			delete_to_trash = true,
+			view_options = {
+				show_hidden = true,
+				is_always_hidden =
+					function(name, _)
+						return name == '..' or name == '.git' or name == '.jj'
+					end,
+			},
 		},
-		-- Optional dependencies
 		dependencies = { { "echasnovski/mini.icons", opts = {} } },
-		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-		-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
 		lazy = false,
-		keys = { { "n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" } }, },
 	},
 }
