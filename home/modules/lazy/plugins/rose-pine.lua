cf133a1c93723d1eaf9bd0837b23b620949366ce
Thomas Christensen <thomas@chrstnsn.dk>
2025-08-22 15:02:43 +0200

    lazy: fix rose-pine

--- home/modules/lazy/plugins/rose-pine.lua
+++ home/modules/lazy/plugins/rose-pine.lua
@@ -1,7 +1,23 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		vim.cmd("colorscheme rose-pine")
-	end
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require("rose-pine").setup({
+extend_background_behind_borders = true,
+				styles = {
+					bold = true,
+					italic = true,
+					transparency = true,
+				}
+			})
+			vim.cmd("colorscheme rose-pine")
+		end
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "rose-pine",
+		},
+	},
 }
