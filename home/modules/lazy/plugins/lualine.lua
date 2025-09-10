16927da7396a46cfe7dc0c7888b9c469ea09500a
Thomas Christensen <thomas@chrstnsn.dk>
2025-09-10 18:54:05 +0200

    lazy/lualine: set more context on file paths

--- home/modules/lazy/plugins/lualine.lua
+++ home/modules/lazy/plugins/lualine.lua
@@ -1,9 +1,12 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	opts = {
-		options = {
-			component_separators = "",
-			section_separators = { left = "", right = "" },
-		},
-	},
+	opts = function(_, opts)
+		opts.options.component_separators = ""
+		opts.options.section_separators = { left = "", right = "" }
+
+		local section_c = opts.sections.lualine_c
+		section_c[#section_c] = { LazyVim.lualine.pretty_path({ length = 8 }) }
+
+		return opts
+	end,
 }
