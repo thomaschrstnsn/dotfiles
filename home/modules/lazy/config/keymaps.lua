a1b55ebe7dacd25a09705d290c870e26d33af649
Thomas Christensen <thomas@chrstnsn.dk>
2025-09-19 15:03:44 +0200

    lazy: <leader><leader> = save

--- home/modules/lazy/config/keymaps.lua
+++ home/modules/lazy/config/keymaps.lua
@@ -2,7 +2,8 @@ vim.keymap.del("n", "<leader>wd")
 
 local map = vim.keymap.set
 
-map("n", "<leader>w", "<cmd>w<cr><esc>", { desc = "Save File" })
+-- map("n", "<leader>w", "<cmd>w<cr><esc>", { desc = "Save File" })
+map("n", "<leader><leader>", "<cmd>w<cr><esc>", { desc = "Save File" })
 map("n", "-", "<cmd>Oil<cr>", { desc = "Open Oil file-browser" })
 map("n", "H", function()
 	vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
