38cb4cf5ac532e536e900ee22a1a7ef5f8a044b3
Thomas Christensen <thomas@chrstnsn.dk>
2025-09-25 09:20:13 +0200

    lazy/rest: up response size limit

--- home/modules/lazy/plugins/rest.lua
+++ home/modules/lazy/plugins/rest.lua
@@ -5,6 +5,10 @@ return {
 		keys = {
 			{ "<S-CR>", "<cmd>lua require('kulala').run()<cr>", desc = "Send the request", ft = "http" },
 		},
-		opts = {},
+		opts = {
+			ui = {
+				max_response_size = 10 * 1024 * 1024, -- 10 MB
+			},
+		},
 	},
 }
