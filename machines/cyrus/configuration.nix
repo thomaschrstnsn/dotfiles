f290e65335f4ac0ead784671b85fdf25d979c1e6
Thomas Christensen <thomas@chrstnsn.dk>
2025-10-19 20:31:38 +0200

    cyrus: cleanup

--- machines/cyrus/configuration.nix
+++ machines/cyrus/configuration.nix
@@ -154,22 +154,6 @@
   security.rtkit.enable = true;
   security.polkit.enable = true;
 
-  # systemd = {
-  #   user.services.polkit-gnome-authentication-agent-1 = {
-  #     description = "polkit-gnome-authentication-agent-1";
-  #     wantedBy = [ "graphical-session.target" ];
-  #     wants = [ "graphical-session.target" ];
-  #     after = [ "graphical-session.target" ];
-  #     serviceConfig = {
-  #       Type = "simple";
-  #       ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-  #       Restart = "on-failure";
-  #       RestartSec = 1;
-  #       TimeoutStopSec = 10;
-  #     };
-  #   };
-  # };
-  #
   services.pipewire = {
     enable = true;
     alsa.enable = true;
@@ -177,18 +161,6 @@
     pulse.enable = true;
     jack.enable = true;
   };
-
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-    input = {
-      General = {
-        UserspaceHID = true;
-      };
-    };
-  };
-
-  services.blueman.enable = true;
 
   services.openssh.enable = true;
 
