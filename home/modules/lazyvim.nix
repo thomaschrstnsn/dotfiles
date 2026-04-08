fc10415adbfeab933c53cb4141eeb56ac1546282
Thomas Christensen <thomas@chrstnsn.dk>
2026-04-08 13:49:01 +0200

    mermaid from nixpkgs also on darwin

--- home/modules/lazyvim.nix
+++ home/modules/lazyvim.nix
@@ -113,7 +113,7 @@ in
             markdownlint-cli2
           ])
           (mkIfList cfg.lang.markdown.zk.enable [ imagemagick ])
-          (mkIfList (cfg.lang.markdown.zk.enable && pkgs.stdenv.isLinux) [ mermaid-cli ]) # aarch64-darwin needs wrapper for unpackaged chrome, see ../../darwin/mermaid-cli.nix
+          (mkIfList cfg.lang.markdown.zk.enable [ mermaid-cli ])
           (mkIfList (cfg.lang.markdown.zk.enable && pkgs.stdenv.isDarwin) [ pngpaste ])
           (mkIfList cfg.lang.json.enable [ vscode-langservers-extracted ])
           (mkIfList cfg.lang.typescript.enable [ typescript-language-server vtsls ])
