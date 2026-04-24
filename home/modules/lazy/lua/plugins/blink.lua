152e6190ca679d8908f9b4a973e029021cbdf856
Thomas Christensen <thomas@chrstnsn.dk>
2026-04-24 14:00:17 +0200

    lazy/blink: tweak shortcut to avoid clash with tmux (and format)

--- home/modules/lazy/lua/plugins/blink.lua
+++ home/modules/lazy/lua/plugins/blink.lua
@@ -3,10 +3,10 @@ return {
 	opts = function(_, opts)
 		opts.keymap = opts.keymap or {}
 		opts.keymap = {
-			['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-			['<C-e>'] = { 'hide', 'fallback' },
+			["<C-.>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide", "fallback" },
 
-			['<Tab>'] = {
+			["<Tab>"] = {
 				function(cmp)
 					if cmp.snippet_active() then
 						return cmp.accept()
@@ -14,20 +14,20 @@ return {
 						return cmp.select_and_accept()
 					end
 				end,
-				'snippet_forward',
-				'fallback'
+				"snippet_forward",
+				"fallback",
 			},
-			['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+			["<S-Tab>"] = { "snippet_backward", "fallback" },
 
-			['<Up>'] = { 'select_prev', 'fallback' },
-			['<Down>'] = { 'select_next', 'fallback' },
-			['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
-			['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+			["<Up>"] = { "select_prev", "fallback" },
+			["<Down>"] = { "select_next", "fallback" },
+			["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+			["<C-n>"] = { "select_next", "fallback_to_mappings" },
 
-			['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-			['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+			["<C-b>"] = { "scroll_documentation_up", "fallback" },
+			["<C-f>"] = { "scroll_documentation_down", "fallback" },
 
-			['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
+			["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
 		}
 		return opts
 	end,
