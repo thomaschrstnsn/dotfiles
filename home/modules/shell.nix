53d882827e54eeb9c9d863789f4008c9fe1b8103
Thomas Christensen <thomas@chrstnsn.dk>
2026-04-28 22:04:34 +0200

    bat: bump rosepine theme

--- home/modules/shell.nix
+++ home/modules/shell.nix
@@ -85,8 +85,8 @@ in
           rose-pine-src = pkgs.fetchFromGitHub {
             owner = "rose-pine";
             repo = "tm-theme";
-            rev = "417d201beb5f0964faded5448147c252ff12c4ae";
-            sha256 = "sha256-aNDOqY81FLFQ6bvsTiYgPyS5lJrqZnFMpvpTCSNyY0Y";
+            rev = "23bb25b9c421cdc9ea89ff3ad3825840cd19d65d";
+            sha256 = "sha256-GUFdv5V5OZ2PG+gfsbiohMT23LWsrZda34ReHBr2Xy0";
           };
         in
         {
