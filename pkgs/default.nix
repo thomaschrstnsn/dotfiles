0ea683389f52cc5159800f63265439f65847bcd1
Thomas Christensen <thomas@chrstnsn.dk>
2026-04-28 22:13:36 +0200

    remove custom github-copilot-cli

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,7 +4,6 @@ with pkgs;
   myPkgs = {
     aeroTrafficControl = callPackage ./aetc.nix { };
     appleFonts = import ./apple-fonts { inherit pkgs; };
-    github-copilot-cli = callPackage ./github-copilot-cli { };
     hyprfocus = callPackage ./hyprfocus.nix { };
     pyenv-nix-install = inputs.pyenv-nix-install.packages.${system}.default;
     screentime-web = callPackage ./screentime-web.nix { };
