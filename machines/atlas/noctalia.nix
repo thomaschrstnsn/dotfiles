4abe1b1ec4116a6537e24b32a01ad1660461f1e5
Thomas Christensen <thomas@chrstnsn.dk>
2026-06-07 21:36:57 +0200

    atlas: noctalia notifications on HDMI

--- machines/atlas/noctalia.nix
+++ machines/atlas/noctalia.nix
@@ -3,6 +3,7 @@
   home-manager.users.thomas = {
     programs.noctalia-shell = {
       enable = true;
+      # noctalia-shell ipc call state all | jq .settings
       settings = builtins.fromJSON (builtins.readFile ./noctalia/settings.json);
     };
   };
